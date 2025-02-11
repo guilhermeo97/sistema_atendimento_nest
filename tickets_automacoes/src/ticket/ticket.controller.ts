8d0760d752c183908414f46297353c9cfb4a62fe
Guilherme Oliveira <55219644+guilhermeo97@users.noreply.github.com>
2025-02-11 09:59:25 +0000

    feat: create ticket

--- tickets_automacoes/src/ticket/ticket.controller.ts
+++ tickets_automacoes/src/ticket/ticket.controller.ts
@@ -7,11 +7,14 @@ import {
   Param,
   Delete,
   UseGuards,
+  Req,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { TicketService } from './ticket.service';
 import { CreateTicketDto } from './dto/create-ticket.dto';
 import { UpdateTicketDto } from './dto/update-ticket.dto';
 import { AuthGuard } from '../auth/auth.guard';
+import { Request } from 'express';
 
 @UseGuards(AuthGuard)
 @Controller('ticket')
@@ -19,8 +22,16 @@ export class TicketController {
   constructor(private readonly ticketService: TicketService) {}
 
   @Post('create')
-  create(@Body() createTicketDto: CreateTicketDto) {
-    return this.ticketService.create(createTicketDto);
+  create(@Req() request: Request, @Body() createTicketDto: CreateTicketDto) {
+    try {
+      const token = request.cookies.token;
+      if (!token) {
+        throw new UnauthorizedException('Invalid credentials');
+      }
+      return this.ticketService.create(createTicketDto);
+    } catch (err) {
+      return err;
+    }
   }
 
   @Get('all')
